4d615bf15ebc2bd101475b61e8273d4f6e63b5a6
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-01-03 16:36:44 +0700

    change logo in navbar

--- resources/views/partials/admin/navbar.blade.php
+++ resources/views/partials/admin/navbar.blade.php
@@ -2,9 +2,9 @@
 <nav aria-label="menu nav" class="bg-gray-800 pt-2 md:pt-1 pb-3 md:pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
 
     <div class="flex flex-wrap items-center justify-between">
-        <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
+        <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white md:ml-2 pt-3">
             <a href="#" aria-label="Home">
-                <span class="text-xl pl-2"><i class="em em-grinning"></i></span>
+                <img src="{{ asset('storage/assets/logo/' . $logo) }}" class="h-10" alt="">
             </a>
         </div>
 
@@ -90,15 +90,6 @@
                             </svg></button>
                         <div id="manajemen-nav"
                             class="dropdownlist absolute bg-gray-800 text-white -left-10 md:-left-20 mt-3 p-3 overflow-auto z-30 invisible">
-                            <input type="text" class="drop-search p-2 text-gray-600" placeholder="Search.."
-                                id="myInput" onkeyup="filterDD('myDropdown','myInput')">
-                            <a href="#"
-                                class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i
-                                    class="fa fa-user fa-fw"></i> Profile</a>
-                            <a href="#"
-                                class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i
-                                    class="fa fa-cog fa-fw"></i> Settings</a>
-                            <div class="border border-gray-800"></div>
                             <a href="{{ route('logout') }}"
                                 class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i
                                     class="fas fa-sign-out-alt fa-fw"></i> Log Out</a>
