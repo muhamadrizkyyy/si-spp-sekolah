005dbd61c477ceb5756f37e51abe5bd0c640c145
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-01-11 17:54:08 +0700

    add back-btn

--- resources/views/livewire/admin/tahun-ajaran/form-tahun-ajaran.blade.php
+++ resources/views/livewire/admin/tahun-ajaran/form-tahun-ajaran.blade.php
@@ -27,7 +27,15 @@
                 </p>
             @enderror
         </div>
-        <button type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <div class="btn-group">
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Submit
+            </button>
+            <a href="{{ route('tahunAjaran.index') }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Kembali
+            </a>
+        </div>
     </form>
 </div>
