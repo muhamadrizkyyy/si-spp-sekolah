6ffd9fe34d1ce1e4d13f340b80924b9435c12405
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-01-11 18:22:25 +0700

    Change style login

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\identitasWeb;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
@@ -9,22 +10,28 @@ class Login extends Component
 {
     public $email;
     public $password;
+    public $logo;
+
+    public function mount()
+    {
+        $this->logo = identitasWeb::first()->logo;
+    }
 
     public function login()
     {
-        $this->validate([
+        $validated = $this->validate([
             "email" => "required|email",
             "password" => "required"
         ]);
 
-        if (Auth::attempt($this->all())) {
+        if (Auth::attempt($validated)) {
             return redirect()->route("dashboardAdmin");
         }
     }
 
     public function render()
     {
-        return view('livewire.auth.login')->layout("layouts.auth");
+        return view('livewire.auth.login')->layout("layouts.auth", ["logo" => $this->logo]);
     }
 
     public function logout()
