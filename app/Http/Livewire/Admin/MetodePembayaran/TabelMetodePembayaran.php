0e93e921116a150da54be521c665c253e073ca4e
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-02-04 22:04:00 +0700

    ubah logika menghapus file yang sudah terupload menggunakan Storage::delete

--- app/Http/Livewire/Admin/MetodePembayaran/TabelMetodePembayaran.php
+++ app/Http/Livewire/Admin/MetodePembayaran/TabelMetodePembayaran.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire\Admin\MetodePembayaran;
 
 use App\Models\MetodePembayaran;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class TabelMetodePembayaran extends Component
 {
     public $metode_pembayaran;
+    public $listeners = ["deleteAct" => 'delete']; //property untuk menangkap event dari emit component lain / dari frontend (js) secara global
 
     public function mount()
     {
@@ -17,8 +19,13 @@ class TabelMetodePembayaran extends Component
     public function delete($id)
     {
         $metode_pembayaran = MetodePembayaran::find($id);
+        $logo = $metode_pembayaran->logo;
+
         if ($metode_pembayaran) {
             $metode_pembayaran->delete();
+
+            // Hapus file logo lama (use storage:link yang dihapus file di folder storagenya)
+            Storage::delete("public/assets/pay-logo/" . $logo);
         }
     }
 
